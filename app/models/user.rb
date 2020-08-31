9129571e941220cf57f894ad1487222e158c90c6
KoshiKajimura <absolute_zero_degree000@yahoo.co.jp>
2020-08-31 19:32:25 +0900

    本人確認のバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,9 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'must use mixed characters'}
+  validates :family_name, presence: true, format:{with: /\A[ぁ-んァ-ン一-龥]/, message: 'must use only hull characters'}
+  validates :first_name, presence: true, format:{with: /\A[ぁ-んァ-ン一-龥]/, message: 'must use only hull characters'}
+  validates :family_name_cana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/, message: 'must use only hull Cana characters'}
+  validates :first_name_cana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/, message: 'must use only hull Cana characters'}
+  validates :birthday, presence: true
 end
