c77a47806705f49f420f1990318decfee35ffa58
KoshiKajimura <absolute_zero_degree000@yahoo.co.jp>
2020-08-31 18:20:10 +0900

    FactoryBot、Faler、Gimeiの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
 end
