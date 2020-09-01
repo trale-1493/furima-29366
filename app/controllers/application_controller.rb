6c04a3a7ad27ac16e95f1927f36b297dde01421c
KoshiKajimura <absolute_zero_degree000@yahoo.co.jp>
2020-09-01 15:49:42 +0900

    サインイン状態の管理機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
   
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
-    end
-  end
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name])
@@ -15,5 +10,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name_cana])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_cana])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  # has_many :products
-  # has_many :purchases
-  # has_many :comments
-  # has_many :favorites
-  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
