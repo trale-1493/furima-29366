3f7d569a987c69e5e2130d4822e962b8beb527ab
KoshiKajimura <absolute_zero_degree000@yahoo.co.jp>
2020-09-01 20:11:09 +0900

    商品出品機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,19 @@ class ProductsController < ApplicationController
 
   def index
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    Product.create(product_params)
+  end
+
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :image, :value, :explain, :category_id, :condition_id,
+                                    :delivery_payer_id, :region_id, :delivery_span_id).merge(user_id: current_user.id)
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,14 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
-  has_one_active_hash :condition
-  has_one_active_hash :delivery_payer
-  has_one_active_hash :region
-  has_one_active_hash :delivery_span
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :delivery_payer
+  belongs_to_active_hash :region
+  belongs_to_active_hash :delivery_span
 
   validates :name, :image, :value, :explain, :category_id,
             :condition_id, :delivery_payer_id, :region_id, :delivery_span_id, presence: true
-  validates :category_id, :condition_id, :delivery_payer_id, :re numericality: {other_than: 1}
+  validates :category_id, :condition_id, :delivery_payer_id, :region_id, :delivery_span_id, numericality: {other_than: 1}
 
   belongs_to :user
-  has_one :purchase
-  has_many :comments
-  has_many :favorites
 end
