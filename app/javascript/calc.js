a2942cdc8b6b83e18d29b7f2f1dabb4e388d0cfa
KoshiKajimura <absolute_zero_degree000@yahoo.co.jp>
2020-09-02 17:06:06 +0900

    手数料や販売利益の非同期的な計算を実装

---
+++ app/javascript/calc.js
@@ -0,0 +1,15 @@
+function calc() {
+  const value_input = document.getElementById("item-price");
+  value_input.addEventListener("input", function(){
+    const tax_output = document.getElementById("add-tax-price");
+    const profit_output = document.getElementById("profit");
+     
+    var price = Math.floor(this.value);
+    var tax = Math.floor(price * 0.1);
+    var profit = price - tax;
+
+    tax_output.innerHTML = tax
+    profit_output.innerHTML = profit
+  });
+}
+window.addEventListener("load", calc)
