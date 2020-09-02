79d45f027273b68e7fd814aba90a31a028e1d0ec
KoshiKajimura <absolute_zero_degree000@yahoo.co.jp>
2020-09-02 18:14:00 +0900

    product単体テストに用いる FacctoryBotの実装

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :product do
-    
+    name              {Faker::Lorem.word}
+    value             {Faker::Number.between(from: 300, to: 9999999)}
+    explain           {Faker::Lorem.sentence}
+    category_id       {Faker::Number.between(from: 2, to: 11)}
+    condition_id      {Faker::Number.between(from: 2, to: 7)}
+    delivery_payer_id {Faker::Number.between(from: 2, to: 3)}
+    region_id         {Faker::Number.between(from: 2, to: 48)}
+    delivery_span_id  {Faker::Number.between(from: 2, to: 4)}
+    association    :user
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @product = FactoryBot.build(:product)
+    @product.image = fixture_file_upload('public/images/test_image.png')
+  end
+
+  describe '商品の新規登録' do
+    it "カラムの情報が揃っていれば新規登録に成功する" do
+        expect(@product).to be_valid
+    end
+  end
 end
