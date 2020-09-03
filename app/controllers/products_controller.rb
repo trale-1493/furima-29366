7a98e880c1187e91da000eae8ff869cb6e439d86
KoshiKajimura <absolute_zero_degree000@yahoo.co.jp>
2020-09-03 16:05:58 +0900

    商品編集機能を実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,7 +26,12 @@ class ProductsController < ApplicationController
   end
 
   def update
-
+    product = Product.find(params[:id])
+    if product.update(product_params)
+      redirect_to product_path(product.id)
+    else
+      render :edit
+    end
   end
 
   private
