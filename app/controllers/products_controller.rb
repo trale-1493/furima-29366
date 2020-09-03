8ae4e946da6624ea9c673d79387077af3e91ee80
KoshiKajimura <absolute_zero_degree000@yahoo.co.jp>
2020-09-03 17:08:47 +0900

    set_productの実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :move_to_signin, only: [:new, :create, :edit, :update]
-  before_action :check_id
+  before_action :set_product, only: [:show, :edit, :update]
   def index
     @products = Product.includes(:user, :purchase)
   end
@@ -19,18 +19,15 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
     redirect_to product_path(@product.id) unless current_user.id == @product.user.id
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
-      redirect_to product_path(product.id)
+      redirect_to product_path(@product.id)
     else
       render :edit
     end
@@ -47,7 +44,7 @@ class ProductsController < ApplicationController
     redirect_to new_user_session_path unless user_signed_in?
   end
 
-  def check_id
-
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
