79250cec79c85537d86e2427f0b011a5c6fedae0
KoshiKajimura <absolute_zero_degree000@yahoo.co.jp>
2020-09-04 12:36:29 +0900

    商品購入機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -7,6 +7,23 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @product = Product.find(params[:product_id])
+    @purchase = @product.build_purchase(purchase_params)
+    @address = @purchase.build_address(address_params)
+    if @address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
 
+  private
+  def purchase_params
+    params.permit.merge(user_id: current_user.id)
+  end
+
+  def address_params
+    params[:purchase].permit(:postal_code, :region_id, :city, :city_number,
+                             :building, :telephone_number)
   end
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,7 +1,4 @@
 class Purchase < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :region
-
   belongs_to :user
   belongs_to :product
   has_one :address
