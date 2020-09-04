a94f8a23a753bb456f427babe39a8018ad468931
KoshiKajimura <absolute_zero_degree000@yahoo.co.jp>
2020-09-04 11:04:31 +0900

    purchaseコントローラーとindex、createアクションの実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'products#index'
 
-  resources :products
+  resources :products do
+    resources :purchases, only: [:index, :create]
+  end
 end
