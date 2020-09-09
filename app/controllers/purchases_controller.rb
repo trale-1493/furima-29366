102a311f8532cd74a721eb4d2fa124aacabc38bf
KoshiKajimura <absolute_zero_degree000@yahoo.co.jp>
2020-09-09 16:02:10 +0900

    秘密鍵、公開鍵を環境変数の参照に再度変更
    以前公開した公開鍵、秘密鍵を変更したため

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -23,7 +23,7 @@ class PurchasesController < ApplicationController
   end
 
   def pay_product
-    Payjp::api_key = "sk_test_d8fd7b8ebc89ed98620dc438"
+    Payjp::api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: @product.value,
       card: params[:token],
