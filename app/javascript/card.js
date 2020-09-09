be3d8c3ebf1a87599a6a3c1f20c7de4b63121319
KoshiKajimura <absolute_zero_degree000@yahoo.co.jp>
2020-09-09 15:51:35 +0900

    Revert "PAYJPの鍵を環境変数の参照に変更"
    This reverts commit d57b4cd8ea4027aac2aa944d7e3907c8d6161557.

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
-
+  Payjp.setPublicKey("pk_test_3f8d9beaa5352bcafd83bb2c");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
