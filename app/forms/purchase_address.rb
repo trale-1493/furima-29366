4d0f5d8cf654a001c3ae141e6f1cc0bd1255b775
KoshiKajimura <absolute_zero_degree000@yahoo.co.jp>
2020-09-09 12:19:52 +0900

    バリデーションの実装

--- app/forms/purchase_address.rb
+++ app/forms/purchase_address.rb
@@ -3,6 +3,15 @@ class PurchaseAddress
 
   attr_accessor :token, :user_id, :product_id, :purchase_address, :postal_code, :region_id, :city, :city_number, :building, :telephone_number
 
+  validates :token, presence: true
+  validates :user_id, presence: true
+  validates :product_id, presence: true
+  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: 'invalid format.'}
+  validates :region_id, presence: true, numericality: { other_than: 1 }
+  validates :city, presence: true
+  validates :city_number, presence: true
+  validates :telephone_number, presence: true, format: { with: /\A\d{,11}\z/, message: "can't use except numbers"}
+
   def save
     return false if invalid?
 
