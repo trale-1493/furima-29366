72d309cde24ec3be2d794a29477c34c9bcfc0430
KoshiKajimura <absolute_zero_degree000@yahoo.co.jp>
2020-09-14 19:11:59 +0900

    商品購入日の表示を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.id == current_user.id
-      @purchased_products = Product.find(@user.purchases.pluck(:product_id))
+      @purchases = @user.purchases.includes(:product)
     end
   end
 end
