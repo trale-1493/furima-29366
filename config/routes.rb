e3cfef903f7be7e172aaf32c653bf0b196b50f5a
KoshiKajimura <absolute_zero_degree000@yahoo.co.jp>
2020-09-14 14:15:42 +0900

    Usersコントローラーの実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'products#index'
 
+  resources :users, only: :show
   resources :products do
     resources :purchases, only: [:index, :create]
 
     collection do
       get 'search'
     end
-  end
+  end  
 end
